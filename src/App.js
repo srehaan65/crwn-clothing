51d38753927d29dcd029088811fca13acc52b34e
srehaan65 <srehaan65@nimapinfotech.com>
2019-11-17 10:47:40 +0530

    added routing structure to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './App.css';
+import {Switch,Route} from 'react-router-dom'
+import Homepage from './pages/homepage/homepage.component'
 
-import Homepage from './homepage.component'
+const HatsPage=(props)=>{
+  console.log(props);
+  return(
+    <div>
+      <h2>HATS PAGE</h2>
+    </div>
+  );
+  
+}
 
 function App() {
   return (
     <div>
-       <Homepage />
+       {/* <Homepage /> */}
+       <Switch>
+           <Route exact path="/"  component={Homepage}></Route>
+            <Route   path="/hats"  component={HatsPage}></Route>
+       </Switch>
     </div>
   );
 }
